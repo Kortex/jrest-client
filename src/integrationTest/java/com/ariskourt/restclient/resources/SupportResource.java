529b660004cc3ee9a8173537dd27822032dfe683
Aris Kourtesas <aris.kourtesas@foodtecsolutions.com>
2021-05-28 17:30:25 +0300

    changes

---
+++ src/integrationTest/java/com/ariskourt/restclient/resources/SupportResource.java
@@ -0,0 +1,13 @@
+package com.ariskourt.restclient.resources;
+
+public class SupportResource {
+
+    private String url;
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    private String text;
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+}
